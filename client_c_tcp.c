d609e1cbb25a910117973dab850b8ebba6aab645
Myles Adams <myles.n.adams@gmail.com>
2018-10-23 04:24:11 -0700

    Started TCP server/client, they are connected, but no functionality

--- client_c_tcp.c
+++ client_c_tcp.c
@@ -1,6 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <limits.h>
+#include <ctype.h>
 
-int main()
+#define BUFFERSIZE 128
+
+int main(int argc, char **argv)
 {
+  if (argc < 3)
+  {
+    perror("Invalid number of arguments");
+    exit(EXIT_FAILURE);
+  }
+
+  int Sockfd;
+  char Buffer[BUFFERSIZE];
+
+  struct sockaddr_in ServAddr;
+
+  int Port = strtol(argv[2], (char **)NULL, 10);
+
+  // Creating socket file descriptor
+  if ((Sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    perror("socket creation failed");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&ServAddr, 0, sizeof(ServAddr));
+
+  // Filling server information
+  ServAddr.sin_family = AF_INET;
+  ServAddr.sin_port = htons(Port);
+  ServAddr.sin_addr.s_addr = inet_addr(argv[1]);
+
+  if (connect(Sockfd, (struct sockaddr *)& ServAddr, sizeof(ServAddr)) != 0)
+  {
+    perror("Failed to connect to server");
+    exit(EXIT_FAILURE);
+  }
+
+  char* hello = "Hey there, how's it going\0";
+
+  write(Sockfd , hello , strlen(hello));
+  printf("Hello message sent\n");
+  int valread = read(Sockfd, Buffer, BUFFERSIZE);
+
+  printf("Buffer: %s\n", Buffer);
+
+
+
+
   return 0;
 }
--- server_c_tcp.c
+++ server_c_tcp.c
@@ -1,6 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <ctype.h>
+#include <errno.h>
+#include <limits.h>
 
-int main()
+#define BUFFERSIZE 128
+
+int main(int argc, char **argv)
 {
+  if (argc < 2)
+  {
+    perror("Invalid number of arguments");
+    exit(EXIT_FAILURE);
+  }
+
+  int Sockfd, Connfd;
+  char InBuffer[BUFFERSIZE];
+  char OutBuffer[BUFFERSIZE];
+
+  struct sockaddr_in ServAddr, CliAddr;
+
+  int Port = strtol(argv[1], (char **)NULL, 10);
+
+  if ((Sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+    perror("Failed to create socket");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&ServAddr, 0, sizeof(ServAddr));
+  memset(&CliAddr, 0, sizeof(CliAddr));
+
+  ServAddr.sin_family = AF_INET;
+  ServAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  ServAddr.sin_port = htons(Port);
+
+  if (setsockopt(Sockfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
+  {
+    perror("setsockopt(SO_REUSEADDR) failed");
+    exit(EXIT_FAILURE);
+  }
+
+  if (bind(Sockfd, (const struct sockaddr *)& ServAddr, sizeof(ServAddr)) < 0)
+  {
+    perror("Failed to bind");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(Sockfd, 3) < 0)
+  {
+    perror("Listening error");
+    exit(EXIT_FAILURE);
+  }
+
+  socklen_t len = sizeof(CliAddr);
+
+  if ((Connfd = accept(Sockfd, (struct sockaddr *)&CliAddr,
+                           (socklen_t*)&len))<0) 
+  {
+    perror("Accepting error");
+    exit(EXIT_FAILURE);
+  }
+
+  char* hello = "Hello there, how is it going as well?\0";
+
+  read(Connfd, InBuffer, sizeof(InBuffer));
+
+  printf("Inbuffer: %s\n", InBuffer);
+
+  write(Connfd, hello, strlen(hello));
+
   return 0;
 }
+
